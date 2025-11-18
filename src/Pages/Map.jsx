39bd5de8544130c5fbb13857c118f93db6cb7b00
Glyptchi <pikikutime@gmail.com>
2025-11-18 14:08:54 +0700

    Added working longitude latitude, change starting poin to GSP and change the zoom in-out to bottom right.

--- src/Pages/Map.jsx
+++ src/Pages/Map.jsx
@@ -1,42 +1,48 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import L from 'leaflet';
 import '../App.css';
+import '../Pages/Map.css'
 
 import 'leaflet/dist/leaflet.css';
 import '@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css';
-
 import '@geoman-io/leaflet-geoman-free';
 
 const Map = () => {
-    const mapContainer = useRef(null); // Ref to the DOM element
-    const mapInstance = useRef(null);  // Ref to store the map instance
+    const mapContainer = useRef(null);
+    const mapInstance = useRef(null);
+
+    const [coords, setCoords] = useState({ lat: null, lng: null });
 
     useEffect(() => {
         if (mapInstance.current) return;
 
-        mapInstance.current = L.map(mapContainer.current).setView([51.505, -0.09], 13);
+        mapInstance.current = L.map(mapContainer.current, {zoomControl: false}).setView([-7.769912033980343, 110.37818386065933], 16);
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            attribution: '&copy; OpenStreetMap contributors'
+        }).addTo(mapInstance.current);
+
+        L.control.zoom({
+            position: 'bottomright'
         }).addTo(mapInstance.current);
 
         mapInstance.current.pm.addControls({
-            position: 'topleft',
-            drawMarker: true,
-            drawCircleMarker: true,
-            drawPolyline: true,
-            drawRectangle: true,
-            drawPolygon: true,
-            drawCircle: true,
-            editMode: true,
-            dragMode: true,
-            cutPolygon: true,
-            removalMode: true,
-            drawText: true,
+            position: 'topleft'
+        });
+
+        // Aman => listener setelah map ready
+        mapInstance.current.whenReady(() => {
+            mapInstance.current.on("mousemove", (e) => {
+                setCoords({
+                    lat: e.latlng.lat.toFixed(6),
+                    lng: e.latlng.lng.toFixed(6),
+                });
+            });
         });
 
         return () => {
             if (mapInstance.current) {
+                mapInstance.current.off();
                 mapInstance.current.remove();
                 mapInstance.current = null;
             }
@@ -44,12 +50,24 @@ const Map = () => {
     }, []);
 
     return (
-        <div
-            ref={mapContainer}
-            style={{ height: '100%', width: '100%' }}
-        />
+        <>
+            <div className='map-container'
+                ref={mapContainer}
+            />
 
-        
+            <div className="card1">
+                <h4 className='status'>Plan Status</h4>
+                <div className="longlat-row">
+                    <span>Longitude:</span>
+                    <span>{coords.lng ?? "-"}</span>
+                </div>
+                <div className="longlat-row">
+                    <span>Latitude:</span>
+                    <span>{coords.lat ?? "-"}</span>
+                </div>
+            </div>
+                
+        </>
     );
 };
 
